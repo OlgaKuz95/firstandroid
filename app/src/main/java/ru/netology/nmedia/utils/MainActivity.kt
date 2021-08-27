ca8d32a8512828756b1a888da2d26d788edfacbe
Olga Kuzevanova <cuzevanova.olg@yandex.ru>
2021-08-27 22:22:57 +0700

    пока убрала  обработчик событий.

---
+++ app/src/main/java/ru/netology/nmedia/utils/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.utils
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.utils.Calculator
+import ru.netology.nmedia.utils.Post
+
